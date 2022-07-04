916ee171524d249e1d11224d60401d2a28e4bf45
IL <homelanderofseven@gmail.com>
2022-07-04 22:19:37 +0300

    Initial commit

---
+++ SampleAppForXCUITest/SampleAppForXCUITest/SampleAppForXCUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppForXCUITestApp.swift
+//  SampleAppForXCUITest
+//
+//  Created by Ihor Livinskyi on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppForXCUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
